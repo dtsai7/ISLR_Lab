a03e8cf563f42e0ea28e4edd8d1b273f26aeb169
David Tsai <32302812+dtsai7@users.noreply.github.com>
2020-03-08 13:41:41 +0800

    Add files via upload

---
+++ SL_lab_KNN.R
@@ -0,0 +1,11 @@
+### K-Nearest Neighbors
+library(class)
+?knn
+attach(Smarket)
+
+Xlag = cbind(Lag1, Lag2)
+train = Year<2005
+
+knn.pred = knn(Xlag[train,], Xlag[!train,], Direction[train], k=1)
+table(knn.pred, Direction[!train])
+mean(knn.pred==Direction[!train])
